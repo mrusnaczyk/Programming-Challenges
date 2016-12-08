ddfb472c641f65ebd64ffa6fcb3e646a3baae6b6
Mateusz Rusnaczyk <mrusnaczyk@gmail.com>
2016-12-08 14:19:54 -0600

    Completed "Light More Light"

---
+++ Challenges_VII/Light_More_Light/Main.java
@@ -0,0 +1,22 @@
+package Light_More_Light;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		long next = 0;
+		
+		while ((next = s.nextLong()) != 0) {
+			long sqrt = (long) Math.sqrt(next);
+			if (Math.pow(sqrt, 2) == next)
+				System.out.println("yes");
+			else
+				System.out.println("no");
+		}
+		s.close();
+		System.exit(0);
+
+	}
+
+}
