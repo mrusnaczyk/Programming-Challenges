1f5b5211981f554185ca1cf629b2f8350aca6d7a
Mateusz Rusnaczyk <mrusnaczyk@gmail.com>
2016-12-07 19:04:02 -0600

    Completed "Carmichael Numbers"

--- Challenges_VII/Carmichael_Numbers/Main.java
+++ Challenges_VII/Carmichael_Numbers/Main.java
@@ -1,60 +1,64 @@
 package Challenges_VII.Carmichael_Numbers;
 
-import java.math.BigInteger;
-import java.util.HashMap;
 import java.util.Scanner;
 
 public class Main {
-	
+
 	public static final int MAX = 65001;
-	public static HashMap<String,Integer> primeHash;
 
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
-		primeHash = new HashMap<String, Integer>();
 		int next;
-		genPrimes();
-		
-		while((next = s.nextInt()) != 0){
-			BigInteger randNum = BigInteger.valueOf((int) (Math.random() * (next - 1)) + 2);
-			BigInteger fermatTest = randNum.pow(next).mod(BigInteger.valueOf(next));
-			if(fermatTest.equals(randNum) && primeHash.get(String.valueOf(next)) == null){
+
+		while ((next = s.nextInt()) != 0) {
+			boolean isCar = true;
+	        if (checkPrime(next))
+	        	isCar = false;
+	        else{
+		        //check a^b mod b for every value from 2 to 'next' 
+		        for (int i = 2; i < next; i++) {
+		            if (powerMod(i, next, next) != i) {
+		            	isCar = false;
+		            	break;
+		            }
+		        }
+	        }
+	        
+			if (isCar)
 				System.out.println("The number " + next + " is a Carmichael number.");
-			}else{
+			else
 				System.out.println(next + " is normal.");
-			}
 		}
+
 		s.close();
 		System.exit(0);
 
 	}
-	
-	
-	public static void genPrimes(){
-		
-	    boolean[] sieve = new boolean[MAX+1]; 
-	    sieve[0] = true; 
-	    sieve[1] = true; 
-	    sieve[2] = false;
 
-	    for(int i = 4; i <= MAX; i+=2)
-	    	sieve[i] = true;
-	    
-	    for(int i = 3; i < (int)Math.sqrt(MAX)+1; i+=2)
-	    {
-	        if(!sieve[i])
-	        {
-	            for(int j = i*i; j <= MAX; j+=i)
-	            	sieve[j] = true;
-	        }
-	    }
-	    
-	    for(int b = 0; b < sieve.length; b++){
-	    	if(!sieve[b]){
-	    		primeHash.put(String.valueOf(b), b);
-	    	}
-	    }
+    public static boolean checkPrime(int num) {
+        if (num % 2 == 0) 
+            return false;
+        
+        for (int i = 3; i <= (int) Math.sqrt(num); i += 2) {
+            if (num % i == 0)
+                return false;
+        }
+        
+        return true;
+    }
 
-	}
+    
+    public static int powerMod(long a, long b, long m) {
+        long result = 1;
+        while(b > 0) {
+            if (b % 2 == 1)
+                result = (result * a) % m;
+            
+            b /= 2;
+            a = (a * a) % m;
+        }
+        return (int) result;
+    }
+
 
 }
