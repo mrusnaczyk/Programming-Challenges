30709d4c60d132ccaecc3ebfeafd3a09bd5531b5
Mateusz Rusnaczyk <mrusnaczyk@gmail.com>
2016-12-12 21:19:11 -0600

    Completed "Little Bishops"

--- Challenges_VIII/Little_Bishops/Main.java
+++ Challenges_VIII/Little_Bishops/Main.java
@@ -1,75 +1,99 @@
-package Light_More_Light;
+package Challenges_VIII.Little_Bishops;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-	static ArrayList<String> placedBishops;
-	static int numBishops;
 	static int numPlacements;
 
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
 		int sizeOfBoard = 0;
-		numBishops = 0;
+		int numBishops = 0;
 
-		while ((sizeOfBoard = s.nextInt()) != 0 && (numBishops = s.nextInt()) != 0) {
-			numPlacements = 0;
-			placedBishops = new ArrayList<String>();
-			backtrack(1, sizeOfBoard);
-			System.out.println(numPlacements);
+		while (true) {
+			sizeOfBoard = s.nextInt();
+			numBishops = s.nextInt();
+			if (sizeOfBoard == 0 && numBishops == 0) {
+				s.close();
+				System.exit(0);
+			}
+			
+			int totalPlacements = 0;
+			boolean[] white, black;
 
-		}
-
-	}
-
-	public static void backtrack(int row, int n) {
-		// n is the number of columns on the board
-		// i is the current column
-		if (row <= n) {
-			for (int i = 1; i <= n; i++) {
-				boolean isSafe = true;
-				for (String b : placedBishops) {
-					String[] point = b.split(",");
-					int[] coord = { Integer.parseInt(point[0]), Integer.parseInt(point[1]) };
-
-					if (Math.abs(row - coord[0]) == Math.abs(i - coord[1])) {
-						isSafe = false;
-						break;
-					}
-				}
-
-				// Place a Bishop in the cell if it's safe to do so
-				if (isSafe) {
-					placedBishops.add(String.valueOf(row) + "," + String.valueOf(i));
-					System.out.println("*** " + row + "," + i + " is safe. " + placedBishops.size() + "***");
-					// If the number of placed bishops is the number we
-					// want, increment the number of solutions and remove the last placed bishop.
-					if (placedBishops.size() == numBishops) {
-						numPlacements++;
-						System.out.println("New Solution! Number of sol: " + numPlacements +". " + placedBishops);
-						placedBishops.remove(placedBishops.size() - 1);
-						// break;
-					} else {
-							backtrack(row + 1, n);
-					}
-				} else {
-					System.out.println(row + "," + i + " is not safe.");
-				}
+			//Split board into white and black tiles
+			if (sizeOfBoard % 2 == 0) {
+				white = new boolean[sizeOfBoard - 1];
+				black = new boolean[sizeOfBoard];
+			} else {
+				white = new boolean[sizeOfBoard];
+				black = new boolean[sizeOfBoard - 1];
+			}
+			for (int i = 0; i < numBishops + 1; i++) {
+				numPlacements = 0;
+				backtrack(white, black, i, 1);
+				int a = numPlacements;
 				
-				if(row == n){
-					backtrack(row + 1, n);
-				}
+				numPlacements = 0;
+				backtrack(white, black, numBishops - i, 0);
+				int b = numPlacements;
+				
+				totalPlacements += a * b;
 			}
 
-			if (placedBishops.size() > 0)
-				placedBishops.remove(placedBishops.size() - 1);
-			
-			backtrack(row + 1, n);
-			
+			System.out.println(totalPlacements);
 			
 		}
 
 	}
 
+	static void backtrack(boolean[] white, boolean[] black, int k, int c) {
+		if (k == 0) {
+			numPlacements++;
+			return;
+		}
+		
+		int blackMiddle = (black.length - 1) / 2;
+		int whiteMiddle = (white.length) / 2;
+		int boardSize = white.length + black.length;
+
+		for (int i = c; i < boardSize - k + 1; i += 2) {
+			int temp = i;
+			if (temp > (boardSize/2)) {
+				temp = (boardSize - temp - 1);
+			}
+			temp /= 2;
+
+			/*
+			 * 1)Check if bishop is already placed
+			 * 2)Place the bishop
+			 * 3)Continue searching
+			 * 4)Remove bishop after all possible combinations have been exhausted
+			 */
+			int newStart;
+			int newEnd;
+			
+			if (i % 2 == 0) {
+				newStart = whiteMiddle - temp;
+				newEnd = whiteMiddle + temp + 1;
+				for (int j = newStart; j < newEnd; j++) {
+					if (!white[j]) {
+						white[j] = true;
+						backtrack(white, black, k - 1, i + 2);
+						white[j] = false;
+					}
+				}
+			} else {
+				newStart = blackMiddle - temp;
+				newEnd = blackMiddle + temp + 2;
+				for (int j = newStart; j < newEnd; j++) {
+					if (!black[j]) {
+						black[j] = true;
+						backtrack(white, black, k - 1, i + 2);
+						black[j] = false;
+					}
+				}
+			}
+		}
+	}
 }
